ba012a9325435d0eb8ec63815d233001778ad0bb
Darryl Bayliss <darryl@darrylbayliss.net>
2021-09-19 20:24:12 +0100

    Initial commit.

---
+++ A-Shortfall-Of-Gravitas/A_Shortfall_Of_GravitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Shortfall_Of_GravitasApp.swift
+//  A-Shortfall-Of-Gravitas
+//
+//  Created by Darryl Bayliss on 19/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct A_Shortfall_Of_GravitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
